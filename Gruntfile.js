b9662b760eb5c65d28ed7e4b0d70e8a4e91e5e9c
Charles Müller <charles.mueller@outlook.com>
2021-08-28 14:51:15 +0200

    Drop jQuery

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,16 +14,6 @@ module.exports = function(grunt) {
       }
     },
 
-    concat: {
-      js: {
-        src: [
-          'node_modules/jquery/dist/jquery.min.js',
-          'dist/assets/build.min.js'
-        ],
-        dest: 'dist/assets/build.min.js'
-      }
-    },
-
     copy: {
       favicon: {
         src: 'src/favicon.svg',
@@ -98,7 +88,6 @@ module.exports = function(grunt) {
   });
 
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
@@ -107,7 +96,7 @@ module.exports = function(grunt) {
   grunt.registerTask('css', ['dart-sass']);
   grunt.registerTask('favicon', ['copy:favicon']);
   grunt.registerTask('fonts', ['clean:fonts', 'copy:fonts-inter', 'copy:fonts-icons']);
-  grunt.registerTask('js', ['uglify', 'concat:js']);
+  grunt.registerTask('js', ['uglify']);
   grunt.registerTask('php', ['clean:php', 'copy:php']);
   grunt.registerTask('sql', ['clean:sql', 'copy:sql']);
 
--- src/js/links.js
+++ src/js/links.js
@@ -1,5 +1,7 @@
-$('a').click(function(event) {
-  if ($(this).attr('href') == '#') {
-    event.preventDefault();
-  }
+document.querySelectorAll("a").forEach(function(element) {
+  element.addEventListener("click", function(event) {
+    if (event.target.attributes["href"].value == "#") {
+      event.preventDefault();
+    }
+  });
 });
