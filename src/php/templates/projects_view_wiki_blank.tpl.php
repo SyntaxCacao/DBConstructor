4ff2495528566392827b9d7f468dd0581c61c8b0
Charles Müller <charles.mueller@outlook.com>
2021-09-12 00:03:38 +0200

    Add wiki blankslate text

--- src/php/templates/projects_view_wiki_blank.tpl.php
+++ src/php/templates/projects_view_wiki_blank.tpl.php
@@ -1,6 +1,6 @@
 <main class="blankslate">
   <span class="blankslate-icon bi bi-book"></span>
   <h3 class="blankslate-heading">Es sind noch keine Seiten angelegt.</h3>
-  <p class="blankslate-text">Hier steht eine Erläuterung, die etwas länger ist, und noch fehlt.</p>
+  <p class="blankslate-text">Das Wiki ist eine Möglichkeit, Informationen mit anderen Projektbeteiligten zu teilen.</p>
   <a class="button" href="<?php echo $data["baseurl"] ?>/projects/<?php echo $data["project"]->id ?>/wiki/create/">Erste Seite anlegen</a>
 </main>
