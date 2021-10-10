cb9aee75be48db7d541ea32936b0cf26f3f4fc6b
Charles Müller <charles.mueller@outlook.com>
2021-10-10 19:23:28 +0200

    Fix href="#" click prevention
    Didn't work when clicking an element inside the anchor element

--- src/js/links.js
+++ src/js/links.js
@@ -8,10 +8,10 @@ document.querySelectorAll(".js-confirm").forEach(function (element) {
   });
 });
 
-document.querySelectorAll("a").forEach(function (element) {
-  element.addEventListener("click", function (event) {
-    if ("href" in event.target.attributes && event.target.attributes["href"].value === "#") {
-      event.preventDefault();
-    }
-  });
+document.addEventListener("click", event => {
+  const closest = event.target.closest("a");
+
+  if (closest !== null && "href" in closest.attributes && closest.attributes["href"].value === "#") {
+    event.preventDefault();
+  }
 });
