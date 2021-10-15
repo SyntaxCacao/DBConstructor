4e1f5f0c63e9e3804606c6314fcea237de0ae87c
Charles Müller <charles.mueller@outlook.com>
2021-10-16 00:16:41 +0200

    Set maxLength for RegExField

--- src/php/DBConstructor/Controllers/Projects/Tables/Structure/RegExField.php
+++ src/php/DBConstructor/Controllers/Projects/Tables/Structure/RegExField.php
@@ -14,6 +14,7 @@ class RegExField extends TextField
         parent::__construct($name, "Regulärer Ausdruck");
         $this->dependsOn = $dependsOn;
         $this->dependsOnValue = $dependsValue;
+        $this->maxLength = 350;
         $this->monospace = true;
         $this->placeholder = "/^[a-z]+$/";
         $this->required = false;
