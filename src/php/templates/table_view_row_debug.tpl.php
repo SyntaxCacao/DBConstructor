048fb7ca09f6dfaa0a8574f1f5c7b565e8270b7b
Charles Müller <charles.mueller@outlook.com>
2022-01-06 00:05:39 +0100

    Fix typo in dataset debug view
    Suggested by @HanjoHamann

--- src/php/templates/table_view_row_debug.tpl.php
+++ src/php/templates/table_view_row_debug.tpl.php
@@ -1,12 +1,12 @@
 <main class="container">
-  <?php $header = new \DBConstructor\Util\HeaderGenerator("Datensatzt #".$data["row"]->id);
-        $header->subtitle = "Debug-Ansicht";
-        $header->autoActions[] = [
-          "href" => $data["baseurl"]."/projects/".$data["project"]->id."/tables/".$data["table"]->id."/view/".$data["row"]->id."/",
-          "icon" => "arrow-left",
-          "text" => "Zurück"
-        ];
-        $header->generate(); ?>
+<?php $header = new \DBConstructor\Util\HeaderGenerator("Datensatz #".$data["row"]->id);
+      $header->subtitle = "Debug-Ansicht";
+      $header->autoActions[] = [
+        "href" => $data["baseurl"]."/projects/".$data["project"]->id."/tables/".$data["table"]->id."/view/".$data["row"]->id."/",
+        "icon" => "arrow-left",
+        "text" => "Zurück"
+      ];
+      $header->generate(); ?>
   <div class="row break-md">
     <div class="column width-7 markdown">
       <pre><?php var_dump($data["row"]) ?></pre>
