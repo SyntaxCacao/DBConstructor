7fdf653f9561c709d9b388caff31a76fca895e55
Charles Müller <charles.mueller@outlook.com>
2022-02-09 20:12:11 +0100

    Remove unused template project_routines_list

--- src/php/templates/project_routines_list.tpl.php
+++
@@ -1,5 +0,0 @@
-<main class="blankslate">
-  <span class="blankslate-icon bi bi-pencil"></span>
-  <h1 class="blankslate-heading">Es sind keine Eingaberoutinen angelegt.</h1>
-  <p class="blankslate-text">Komplexe Erfassungsvorgänge können als Eingaberoutinen gespeichert werden.</p>
-</main>
