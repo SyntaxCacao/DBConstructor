SyntaxCacao/DBConstructor

commit 2f9a5b950ae3162e97b9196cda0c634db8a7f123

Refactor RelationalSelectField and table filter
Resolves #81
Resolves #76
Resolves #77
Resolves #103
See #79
See #80

src/js/selector.js
@@ -1,0 +1,175 @@
+function loadTable(modal, searchColumn, searchValue, page) {
+  const modalContent = modal.querySelector(".modal-content");
+  modalContent.innerHTML = '<div class="blankslate"><h3 class="blankslate-text">Laden...</h3></div>';
+  let body = "projectId=" + modal.dataset.projectId + "&page=" + page;
+
+  if (searchColumn !== null) {
+    body += "&searchColumn=" + encodeURIComponent(searchColumn);
+  }
+
+  if (searchValue !== null) {
+    body += "&searchValue=" + encodeURIComponent(searchValue);
+  }
+
+  fetch(document.body.dataset.baseurl + "/api/selector/" + modal.dataset.tableId, {
+    body: body,
+    headers: new Headers({
+      "Content-Type": "application/x-www-form-urlencoded"
+    }),
+    method: "POST",
+    redirect: "manual"
+  }).then(response => {
+    if (response.ok) {
+      response.text().then(text => {
+        modalContent.innerHTML = text;
+        //modalContent.querySelector(".page-table-selector-modal-form-column").focus();
+
+        const pageInput = modalContent.querySelector(".page-table-selector-modal-form-page");
+        const page = parseInt(pageInput.value);
+
+        if (page === 1) {
+          modal.querySelector(".page-table-selector-modal-prev").classList.add("button-disabled");
+        } else {
+          modal.querySelector(".page-table-selector-modal-prev").classList.remove("button-disabled");
+        }
+
+        if (page >= parseInt(pageInput.dataset.pageCount)) {
+          modal.querySelector(".page-table-selector-modal-next").classList.add("button-disabled");
+        } else {
+          modal.querySelector(".page-table-selector-modal-next").classList.remove("button-disabled");
+        }
+      });
+    } else {
+      modalContent.innerHTML = "<p>Error while loading table</p>";
+      console.error("Loading selector table failed");
+    }
+  });
+}
+
+document.addEventListener("openModal", event => {
+  if (event.detail.element.classList.contains("page-table-selector-modal")) {
+    if (event.detail.element.querySelector(".modal-content").childNodes.length === 0) {
+      loadTable(event.detail.element, null, null, 1);
+    }/* else {
+      event.detail.element.querySelector(".page-table-selector-modal-form-column").focus();
+    }*/
+  }
+});
+
+/*
+document.addEventListener("closeModal", event => {
+  if (event.detail.element.classList.contains("page-table-selector-modal")) {
+    document.querySelector("input[name=field-relational-" + event.detail.element.attributes["data-column-id"].value + "]").parentNode.querySelector(".page-table-selector-button").focus();
+  }
+});
+*/
+
+// //
+
+document.addEventListener("click", event => {
+  // refresh button
+  let button = event.target.closest(".page-table-selector-modal-button");
+
+  if (button !== null) {
+    if (button.classList.contains("button-disabled")) {
+      return;
+    }
+
+    button.classList.add("button-disabled");
+    const modal = event.target.closest(".page-table-selector-modal");
+    loadTable(modal, modal.querySelector(".page-table-selector-modal-form-column").value, modal.querySelector(".page-table-selector-modal-form-value").value, modal.querySelector(".page-table-selector-modal-form-page").value);
+    return;
+  }
+
+  // select button
+  button = event.target.closest(".js-table-selector");
+
+  if (button !== null) {
+    const modal = event.target.closest(".page-table-selector-modal");
+    const input = document.querySelector("input[name=field-relational-" + modal.dataset.columnId + "]")
+
+    if (input.value === button.dataset.rowId) {
+      return;
+    }
+
+    if (button.dataset.rowId === "") {
+      input.value = "";
+      input.dataset.valueExists = "0";
+      input.parentNode.querySelector(".page-table-selector-value").innerHTML = 'Keine Auswahl';
+      input.parentNode.querySelector(".page-table-selector-indicator").classList.add("hide");
+      updateRules(input);
+      return;
+    }
+
+    input.dataset.valueExists = "1";
+    input.dataset.valueValid = button.dataset.valid;
+    input.dataset.valueDeleted = button.dataset.deleted;
+    input.value = button.dataset.rowId;
+    input.parentNode.querySelector(".page-table-selector-value").innerHTML = '<a class="main-link" href="' + document.querySelector("body").dataset.baseurl + '/projects/' + modal.dataset.projectId + '/tables/' + modal.dataset.tableId + '/view/' + button.dataset.rowId + '/" target="_blank">#' + button.dataset.rowId + '</a>';
+
+    let icon;
+
+    if (button.dataset.deleted === "1") {
+      icon = "trash";
+    } else if (button.dataset.valid === "1") {
+      icon = "check-lg";
+    } else {
+      icon = "x-lg";
+    }
+
+    input.parentNode.querySelector(".page-table-selector-indicator").classList.remove("hide");
+    input.parentNode.querySelector(".page-table-selector-indicator .bi").className = "bi bi-" + icon;
+
+    updateRules(input);
+    return;
+  }
+
+  // pagination
+  button = event.target.closest(".page-table-selector-modal-prev");
+
+  if (button !== null && ! button.classList.contains("button-disabled")) {
+    const input = event.target.closest(".modal").querySelector(".page-table-selector-modal-form-page");
+    input.value = parseInt(input.value) - 1;
+    input.parentElement.querySelector(".page-table-selector-modal-button").click();
+    return;
+  }
+
+  button = event.target.closest(".page-table-selector-modal-next");
+
+  if (button !== null && ! button.classList.contains("button-disabled")) {
+    const input = event.target.closest(".modal").querySelector(".page-table-selector-modal-form-page");
+    input.value = parseInt(input.value) + 1;
+    input.parentElement.querySelector(".page-table-selector-modal-button").click();
+  }
+});
+
+function updateRules(input) {
+  const rules = document.querySelector(input.parentElement.dataset.rulesElement).querySelectorAll(".validation-step");
+
+  let existsIndex = 0;
+  let validIndex = 1;
+
+  if (input.dataset.nullable === "0") {
+    if (input.value === "") {
+      setFailed(rules.item(0));
+    } else {
+      setSuccess(rules.item(0));
+    }
+
+    existsIndex += 1;
+    validIndex += 1;
+  }
+
+  if (input.value === "") {
+    setSkipped(rules.item(existsIndex));
+    setSkipped(rules.item(validIndex));
+  } else {
+    setSuccess(rules.item(existsIndex));
+
+    if (input.dataset.valueValid === "1" && input.dataset.valueDeleted === "0") {
+      setSuccess(rules.item(validIndex));
+    } else {
+      setFailed(rules.item(validIndex));
+    }
+  }
+}

src/php/DBConstructor/Controllers/Projects/Tables/RelationalSelectField.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace DBConstructor\Controllers\Projects\Tables;
 
+use DBConstructor\Application;
+use DBConstructor\Controllers\Projects\ProjectsController;
 use DBConstructor\Forms\Fields\Field;
 use DBConstructor\Models\RelationalColumn;
 use DBConstructor\Models\RelationalField;
 use DBConstructor\Models\Row;
-use DBConstructor\Models\TextualField;
 use Exception;
 
 class RelationalSelectField extends Field
@@ -16,77 +17,79 @@ class RelationalSelectField extends Field
     /** @var RelationalColumn */
     public $column;
 
-    /** @var string */
+    /** @var string|null */
     public $rowId;
 
-    /** @var array<string, Row> */
-    public $rows;
-
-    /** @var array<string, array<string, TextualField>> */
-    public $table;
+    /** @var Row|null */
+    public $selection;
 
     public function __construct(RelationalColumn $column, string $rowId = null)
     {
         parent::__construct("relational-".$column->id);
         $this->column = $column;
-        $this->rowId = $rowId;
         $this->required = false;
-
-        // TODO: Anders machen!!!!!
-        $this->rows = Row::loadList($this->column->targetTableId);
-        $this->table = TextualField::loadTable($this->column->targetTableId);
+        $this->rowId = $rowId;
     }
 
     public function generateField(): string
     {
-        $html = '<select class="form-select" name="field-'.htmlentities($this->name).'">';
-        $html .= '<option value="">Keine Auswahl</option>';
+        $html = '<input name="field-'.htmlentities($this->name).'" type="hidden"';
 
-        $selectedOptionIncluded = false;
-
-        foreach ($this->table as $id => $fields) {
-            if (! isset($this->rows[$id])) {
-                // This is the case if row has been deleted
-                // TODO Handle this differently
-                continue;
-            }
-
-            $str = "";
-            $first = true;
-
-            foreach ($fields as $field) {
-                if (! $first) {
-                    $str .= "; ";
-                }
-
-                if ($field->value === null) {
-                    $str .= "NULL";
-                } else {
-                    $str .= $field->value;
-                }
-
-                $first = false;
-            }
-
-            if (! $this->rows[$id]->valid) {
-                $str .= " (ungültig)";
-            }
-
-            $html .= '<option value="'.$id.'" data-valid="'.var_export($this->rows[$id]->valid, true).'"';
-
-            if ($this->value == $id) {
-                $html .= ' selected';
-                $selectedOptionIncluded = true;
-            }
-
-            $html .= '>'.htmlentities($str).'</option>';
+        if ($this->value !== null) {
+            $html .= ' value="'.$this->value.'"';
         }
 
-        if ($this->value !== null && ! $selectedOptionIncluded) {
-            $html .= '<option value="'.htmlentities($this->value).'" data-valid="false" selected>Unzulässiger Wert: '.htmlentities($this->value).'</option>"';
+        $html .= ' data-nullable="'.intval($this->column->nullable).'" data-value-exists="'.intval($this->selection !== null).'" data-value-valid="'.intval($this->selection !== null && $this->selection->valid).'" data-value-deleted="'.intval($this->selection !== null && $this->selection->deleted).'">';
+        $html .= '<div class="form-input page-table-selector">';
+
+        if ($this->value === null || $this->selection === null) {
+            $html .= '<span class="page-table-selector-value">';
+
+            if ($this->value === null) {
+                $html .= 'Keine Auswahl';
+            } else {
+                $html .= '#'.htmlentities($this->value);
+            }
+
+            $html .= '</span>';
+            $html .= '<span class="validation-step-icon page-table-selector-indicator hide"><span class="bi bi-check-lg"></span></span>';
+        } else {
+            $html .= '<span class="page-table-selector-value"><a class="main-link" href="'.Application::$instance->config["baseurl"].'/projects/'.ProjectsController::$projectId.'/tables/'.$this->selection->tableId.'/view/'.$this->selection->id.'/" target="_blank">#'.$this->selection->id.'</a></span>';
+            $html .= '<span class="validation-step-icon page-table-selector-indicator">';
+
+            if ($this->selection->deleted) {
+                $html .= '<span class="bi bi-trash"></span>';
+            } else if ($this->selection->valid) {
+                $html .= '<span class="bi bi-check-lg"></span>';
+            } else {
+                $html .= '<span class="bi bi-x-lg"></span>';
+            }
+
+            $html .= '</span>';
         }
 
-        $html .= '</select>';
+        $html .= '<button class="button button-smallest page-table-selector-button js-open-modal" type="button" data-modal="modal-selector-'.$this->column->id.'"><span class="bi bi-pencil no-margin"></span></button>';
+        $html .= '</div>';
+
+        // selection modal
+        $modal = '<div class="modal page-table-selector-modal" id="modal-selector-'.$this->column->id.'" data-project-id="'.ProjectsController::$projectId.'" data-table-id="'.$this->column->targetTableId.'" data-column-id="'.$this->column->id.'">';
+        $modal .= '<div class="modal-container">';
+        $modal .= '<div class="modal-dialog">';
+        $modal .= '<header class="modal-header">';
+        $modal .= '<h3>Datensatz auswählen</h3>';
+        $modal .= '<a class="modal-x js-close-modal" href="#"><span class="bi bi-x-lg"></span></a>';
+        $modal .= '</header>';
+        $modal .= '<div class="modal-content"></div>';
+        $modal .= '<div class="modal-actions">';
+        $modal .= '<a class="button modal-action js-table-selector js-close-modal" href="#" data-row-id="">Keine Auswahl</a>';
+        $modal .= '<a class="button button-disabled modal-action modal-action-right page-table-selector-modal-prev" href="#"><span class="bi bi-arrow-left no-margin"></span>&nbsp;&nbsp;Zurück</a>';
+        $modal .= '<a class="button button-disabled modal-action page-table-selector-modal-next" href="#">Weiter&nbsp;&nbsp;<span class="bi bi-arrow-right no-margin"></span></a>';
+        $modal .= '</div>';
+        $modal .= '</div>';
+        $modal .= '</div>';
+        $modal .= '</div>';
+        Application::$instance->modals[] = $modal;
+
         return $html;
     }
 
@@ -100,10 +103,21 @@ class RelationalSelectField extends Field
         return true;
     }
 
+    public function insertValue($value)
+    {
+        parent::insertValue($value);
+
+        if ($this->value !== null) {
+            $this->selection = Row::load($value);
+        }
+    }
+
     public function validate(): array
     {
-        if (! ($this->value === null || array_key_exists($this->value, $this->rows))) {
-            return ["Wählen Sie eine Option"];
+        if (! ($this->value === null || $this->selection !== null)) {
+            // In this case, there exists a value (= row ID), but no corresponding Row was found
+            // Instead of showing an error message, value will be treated as if no Row was chosen
+            $this->value = null;
         }
 
         if ($this->value !== null && $this->rowId !== null) {

Gruntfile.js
@@ -98,6 +98,8 @@ module.exports = function(grunt) {
             'src/js/links.js',
             'src/js/form-lists.js',
             'src/js/modals.js',
+            'src/js/rowfilter.js',
+            'src/js/selector.js',
             'src/js/tabnav.js',
             'src/js/validation.js'
           ]

src/css/elements/modal.scss
@@ -22,6 +22,7 @@
 
 .modal-container {
   margin: auto;
+  max-width: 100%;
 }
 
 .modal-dialog {
@@ -58,7 +59,7 @@
 
 .modal-x {
   margin-left: auto;
-  padding-left: $spacer-1;
+  padding-left: $spacer-3;
 }
 
 .modal-content {

src/css/layout/utilities.scss
@@ -1,5 +1,9 @@
 // hide utilities //
 
+.hide {
+  display: none !important;
+}
+
 @each $breakpoint, $width in $breakpoints {
   // .show-md: element shown on >= md
   @media (max-width: $width) {

src/css/page/tables.scss
@@ -37,31 +37,52 @@
   margin-bottom: $main-header-margin-bottom;
   margin-top: $main-header-margin-top;
 
-  .button, .form-select {
-    display: block;
-
-    @media (max-width: $breakpoint-md) {
+  @media (max-width: $breakpoint-md) {
+    .button,
+    .form-input,
+    .form-select {
       display: block;
-      margin-bottom: 4px;
+      margin-bottom: $spacer-1;
       width: 100%;
+    }
+
+    .button {
+      text-align: center; // should maybe be a general rule?
     }
   }
 }
 
+.page-table-view-controls-row-expandable:not(.expanded) {
+  display: none;
+}
+
 @media (min-width: $breakpoint-md+1) {
-  .page-table-view-controls {
+  .page-table-view-controls-row {
     display: flex;
 
+    .button,
+    .form-input,
     .form-select {
-      min-width: 0;
-    }
-
-    .button, .form-select {
       &:not(:last-child) {
         margin-right: $spacer-2;
       }
     }
+
+    .form-input,
+    .form-select {
+      flex-grow: 1;
+      min-width: 0;
+    }
   }
+
+  .page-table-view-controls-row-expandable {
+    margin-top: $spacer-2;
+  }
+}
+
+.page-table-view-controls-expand {
+  padding-left: 10px;
+  padding-right: 10px
 }
 
 .page-table-view-icons {
@@ -140,5 +161,82 @@
       overflow-y: auto;
       padding-right: $container-padding;
     }
+  }
+}
+
+// //
+
+.page-table-selector {
+  align-items: center;
+  display: flex;
+  line-height: 30px;
+  padding-bottom: 0;
+  padding-top: 0;
+}
+
+.page-table-selector-value {
+  min-width: 0;
+
+  @include one-line-text;
+}
+
+.page-table-selector-indicator {
+  display: inline-flex;
+  margin-left: $spacer-2;
+}
+
+.page-table-selector-button {
+  margin-left: auto;
+  width: fit-content !important; // important because of .form-block
+}
+
+.page-table-selector-modal {
+  .modal-dialog {
+    max-width: 1000px;
+  }
+
+  .blankslate {
+    padding: $spacer-6 $spacer-5;
+  }
+
+  .blankslate-text {
+    margin-bottom: 0;
+    margin-top: $spacer-1;
+  }
+}
+
+.page-table-selector-modal-form {
+  display: flex;
+}
+
+.page-table-selector-modal-form-column,
+.page-table-selector-modal-form-value,
+.page-table-selector-modal-button {
+  margin-bottom: $spacer-2;
+}
+
+.page-table-selector-modal-form-column,
+.page-table-selector-modal-form-value {
+  margin-right: $spacer-2;
+  min-width: 120px; // just some low value
+}
+
+.page-table-selector-modal-form-value {
+  flex-grow: 1;
+}
+
+@media (max-width: 600px) {
+  .page-table-selector-modal-form {
+    display: initial;
+  }
+
+  .page-table-selector-modal-form-column,
+  .page-table-selector-modal-form-value,
+  .page-table-selector-modal-button {
+    width: 100%;
+  }
+
+  .page-table-selector-modal-button {
+    text-align: center;
   }
 }

src/js/modals.js
@@ -2,6 +2,16 @@ function closeModal(element) {
   document.querySelector("body").classList.remove("modal-open-within");
   element.classList.remove("modal-open");
   //event.target.closest(".modal").classList.remove("modal-open");
+
+  /*
+  const closeEvent = new CustomEvent("closeModal", {
+    bubbles: false,
+    detail: {
+      element: element
+    }
+  });
+  document.dispatchEvent(closeEvent);
+  */
 }
 
 document.addEventListener("click", event => {
@@ -18,8 +28,18 @@ document.addEventListener("click", event => {
   } else {
     // open modal
     if (event.target.closest(".js-open-modal") !== null) {
+      const modal = document.getElementById(event.target.closest(".js-open-modal").attributes["data-modal"].value);
+
+      const openEvent = new CustomEvent("openModal", {
+        bubbles: false,
+        detail: {
+          element: modal
+        }
+      });
+      document.dispatchEvent(openEvent);
+
       document.querySelector("body").classList.add("modal-open-within");
-      document.getElementById(event.target.closest(".js-open-modal").attributes["data-modal"].value).classList.add("modal-open");
+      modal.classList.add("modal-open");
     }
   }
 });

src/js/rowfilter.js
@@ -1,0 +1,25 @@
+document.addEventListener("click", event => {
+  const button = event.target.closest(".page-table-view-controls-expand");
+  if (button !== null) {
+    if (button.classList.contains("button-selected")) {
+      button.classList.remove("button-selected");
+      button.parentNode.parentNode.querySelectorAll(".page-table-view-controls-row-expandable").forEach(row => {
+        row.classList.remove("expanded");
+      });
+    } else {
+      button.classList.add("button-selected");
+      button.parentNode.parentNode.querySelectorAll(".page-table-view-controls-row-expandable").forEach(row => {
+        row.classList.add("expanded");
+      });
+    }
+  }
+});
+
+/*
+document.addEventListener("submit", event => {
+  if (event.target.classList.contains("page-table-view-controls")) {
+    event.target.querySelectorAll("input, select").forEach(element => {
+      element.disabled = element.value === "";
+    });
+  }
+});*/

commit e19a1717aa5801c2a90a9f785f4649e5e2b85e2a

Fix RelationalSelectField not functioning in workflow forms
Caused by 2f9a5b9

src/js/selector.js
@@ -86,7 +86,7 @@ document.addEventListener("click", event => {
 
   if (button !== null) {
     const modal = event.target.closest(".page-table-selector-modal");
-    const input = document.querySelector("input[name=field-relational-" + modal.dataset.columnId + "]")
+    const input = document.querySelector("#page-table-selector-" + modal.dataset.selectorId + "-input")
 
     if (input.value === button.dataset.rowId) {
       return;

src/php/DBConstructor/Controllers/Projects/Tables/RelationalSelectField.php
@@ -14,6 +14,8 @@ use Exception;
 
 class RelationalSelectField extends Field
 {
+    public static $nextId = 1;
+
     /** @var RelationalColumn */
     public $column;
 
@@ -33,7 +35,7 @@ class RelationalSelectField extends Field
 
     public function generateField(): string
     {
-        $html = '<input name="field-'.htmlentities($this->name).'" type="hidden"';
+        $html = '<input id="page-table-selector-'.self::$nextId.'-input" name="field-'.htmlentities($this->name).'" type="hidden"';
 
         if ($this->value !== null) {
             $html .= ' value="'.$this->value.'"';
@@ -68,11 +70,11 @@ class RelationalSelectField extends Field
             $html .= '</span>';
         }
 
-        $html .= '<button class="button button-smallest page-table-selector-button js-open-modal" type="button" data-modal="modal-selector-'.$this->column->id.'"><span class="bi bi-pencil no-margin"></span></button>';
+        $html .= '<button class="button button-smallest page-table-selector-button js-open-modal" type="button" data-modal="modal-selector-'.self::$nextId.'"><span class="bi bi-pencil no-margin"></span></button>';
         $html .= '</div>';
 
         // selection modal
-        $modal = '<div class="modal page-table-selector-modal" id="modal-selector-'.$this->column->id.'" data-project-id="'.ProjectsController::$projectId.'" data-table-id="'.$this->column->targetTableId.'" data-column-id="'.$this->column->id.'">';
+        $modal = '<div class="modal page-table-selector-modal" id="modal-selector-'.self::$nextId.'" data-selector-id="'.self::$nextId.'" data-project-id="'.ProjectsController::$projectId.'" data-table-id="'.$this->column->targetTableId.'">';
         $modal .= '<div class="modal-container">';
         $modal .= '<div class="modal-dialog">';
         $modal .= '<header class="modal-header">';
@@ -90,6 +92,7 @@ class RelationalSelectField extends Field
         $modal .= '</div>';
         Application::$instance->modals[] = $modal;
 
+        self::$nextId += 1;
         return $html;
     }
 