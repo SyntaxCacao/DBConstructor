0795bb87813506e9ab8692bec5f9dee7eaf59f2e
Charles Müller <charles.mueller@outlook.com>
2022-07-15 13:17:44 +0200

    Make form field dependencies work with checkboxes

--- src/js/forms.js
+++ src/js/forms.js
@@ -5,7 +5,22 @@ function updateDependingField(element) {
     groupElement = groupElement.parentElement;
   }
 
-  if (document.querySelector('[name="field-' + element.dataset.dependsOn + '"]').value === element.dataset.dependsOnValue) {
+  const dependency = document.querySelector('[name="field-' + element.dataset.dependsOn + '"]');
+  let show;
+
+  if (dependency.matches("input[type=checkbox]")) {
+    // checkbox
+    if (element.dataset.dependsOnValue === "checked") {
+      show = dependency.checked;
+    } else {
+      show = ! dependency.checked;
+    }
+  } else {
+    // other element
+    show = document.querySelector('[name="field-' + element.dataset.dependsOn + '"]').value === element.dataset.dependsOnValue;
+  }
+
+  if (show) {
     groupElement.classList.add("form-group-depend-show");
   } else if (groupElement.classList.contains("form-group-depend-show")) {
     groupElement.classList.remove("form-group-depend-show");
