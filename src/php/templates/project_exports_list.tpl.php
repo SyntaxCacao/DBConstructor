ef4687fbfe59229d0a7a4c633fcd438578f3f7ee
Charles Müller <charles.mueller@outlook.com>
2025-02-11 18:50:36 +0100

    Use `.table-wrapper` in exports list to improve responsiveness

--- src/php/templates/project_exports_list.tpl.php
+++ src/php/templates/project_exports_list.tpl.php
@@ -36,32 +36,34 @@ use DBConstructor\Util\HeaderGenerator;
       ];
 
       $header->generate(); ?>
-  <table class="table">
-    <tr class="table-heading">
-      <th class="table-cell">ID</th>
-      <th class="table-cell">Format</th>
-      <th class="table-cell">Zeitpunkt</th>
-      <th class="table-cell">Durchgeführt von</th>
-      <th class="table-cell">Bemerkung</th>
-      <th class="table-cell">Dateigröße</th>
-      <th class="table-cell"></th>
-    </tr>
+  <div class="table-wrapper">
+    <table class="table">
+      <tr class="table-heading">
+        <th class="table-cell">ID</th>
+        <th class="table-cell">Format</th>
+        <th class="table-cell">Zeitpunkt</th>
+        <th class="table-cell">Durchgeführt von</th>
+        <th class="table-cell">Bemerkung</th>
+        <th class="table-cell">Dateigröße</th>
+        <th class="table-cell"></th>
+      </tr>
 <?php foreach ($data["exports"] as $export) {
         $existsArchive = $export->existsLocalArchive();
         $existsLocalDir = $export->existsLocalDirectory(); ?>
-    <tr class="table-row">
-      <td class="table-cell table-cell-numeric"><?php echo $export->id; ?></td>
-      <td class="table-cell"><?= htmlentities($export->getFormatLabel()) ?></td>
-      <td class="table-cell"><?= htmlentities(date("d.m.Y H:i", strtotime($export->created))) ?></td>
-      <td class="table-cell"><?= htmlentities($export->userFirstName." ".$export->userLastName) ?><?php if ($export->api) { ?> <span class="table-cell-icon-inline bi bi-robot" title="Durch die API bewirkt"></span><?php } ?></td>
-      <td class="table-cell"><a class="table-link-subtle" href="<?= $data["baseurl"] ?>/projects/<?= $data["project"]->id ?>/exports/<?= $export->id ?>/editnote/" title="Bemerkung bearbeiten"><?= is_null($export->note) ? "&ndash;" : htmlspecialchars($export->note) ?></a></td>
-      <td class="table-cell table-cell-numeric"><?= $existsArchive ? Export::getPrintableFileSize($export->getLocalArchivePath()) : "&ndash;" ?></td>
-      <td class="table-cell table-cell-actions">
-        <a class="button <?= $existsLocalDir ? "" : "button-disabled " ?>button-smallest"<?php if ($existsLocalDir) { ?> href="<?= "{$data["baseurl"]}/projects/{$data["project"]->id}/exports/$export->id/" ?>"<?php } ?>><span class="bi bi-folder2-open"></span>Öffnen</a><!--
-     --><a class="button <?= $existsArchive ? "" : "button-disabled " ?>button-smallest"<?php if ($existsArchive) { ?> href="<?= "{$data["baseurl"]}/exports/$export->id/{$export->getArchiveDownloadName()}" ?>" download<?php } else { ?> title="Die Exportdatei ist auf dem Server nicht mehr vorhanden oder nicht lesbar."<?php } ?>><span class="bi bi-download"></span>Herunterladen</a><!--
-     --><a class="button button-smallest js-confirm" data-confirm-message="Sind Sie sicher?" href="?delete=<?= $export->id ?>"><span class="bi bi-trash3"></span>Löschen</a>
-      </td>
-    </tr>
+      <tr class="table-row">
+        <td class="table-cell table-cell-numeric"><?php echo $export->id; ?></td>
+        <td class="table-cell"><?= htmlentities($export->getFormatLabel()) ?></td>
+        <td class="table-cell"><?= htmlentities(date("d.m.Y H:i", strtotime($export->created))) ?></td>
+        <td class="table-cell"><?= htmlentities($export->userFirstName." ".$export->userLastName) ?><?php if ($export->api) { ?> <span class="table-cell-icon-inline bi bi-robot" title="Durch die API bewirkt"></span><?php } ?></td>
+        <td class="table-cell"><a class="table-link-subtle" href="<?= $data["baseurl"] ?>/projects/<?= $data["project"]->id ?>/exports/<?= $export->id ?>/editnote/" title="Bemerkung bearbeiten"><?= is_null($export->note) ? "&ndash;" : htmlspecialchars($export->note) ?></a></td>
+        <td class="table-cell table-cell-numeric"><?= $existsArchive ? Export::getPrintableFileSize($export->getLocalArchivePath()) : "&ndash;" ?></td>
+        <td class="table-cell table-cell-actions">
+          <a class="button <?= $existsLocalDir ? "" : "button-disabled " ?>button-smallest"<?php if ($existsLocalDir) { ?> href="<?= "{$data["baseurl"]}/projects/{$data["project"]->id}/exports/$export->id/" ?>"<?php } ?>><span class="bi bi-folder2-open"></span>Öffnen</a><!--
+       --><a class="button <?= $existsArchive ? "" : "button-disabled " ?>button-smallest"<?php if ($existsArchive) { ?> href="<?= "{$data["baseurl"]}/exports/$export->id/{$export->getArchiveDownloadName()}" ?>" download<?php } else { ?> title="Die Exportdatei ist auf dem Server nicht mehr vorhanden oder nicht lesbar."<?php } ?>><span class="bi bi-download"></span>Herunterladen</a><!--
+       --><a class="button button-smallest js-confirm" data-confirm-message="Sind Sie sicher?" href="?delete=<?= $export->id ?>"><span class="bi bi-trash3"></span>Löschen</a>
+        </td>
+      </tr>
 <?php } ?>
-  </table>
+    </table>
+  </div>
 </main>
